381a25dd4d3da992be0feadb9623ff79cb0f08cf
thomasdang1996 <thomasdang1996@gmail.com>
2023-08-15 12:35:19 +0200

    add-price: added price column and attribute to entity

--- src/main/java/com/dang/pearstorebe/entity/Product.java
+++ src/main/java/com/dang/pearstorebe/entity/Product.java
@@ -5,15 +5,26 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Entity
 @Getter
 @Setter
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
     private Long id;
+
+    @Column(name = "NAME")
     private String name;
+
+    @Column(name = "PRODUCT_TYPE")
     @Enumerated(EnumType.STRING)
     private ProductType productType;
+
+    @Column(name = "PRICE")
+    private BigDecimal price;
 
     @OneToOne(mappedBy = "product", cascade = CascadeType.ALL)
     private Order order;
