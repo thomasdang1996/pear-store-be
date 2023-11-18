0dc0ffc3433a26ba601f9f05f08148f1f2b73da1
thomasdang1996 <thomasdang1996@gmail.com>
2023-11-18 13:29:21 +0100

    feature: add message-synchronizer

--- src/main/java/com/dang/pearstorebe/accountService/handler/AccountHandler.java
+++ src/main/java/com/dang/pearstorebe/accountService/handler/AccountHandler.java
@@ -3,10 +3,15 @@ package com.dang.pearstorebe.accountService.handler;
 
 import avrogenerated.accountmanager.AccountCreated;
 import avrogenerated.accountmanager.AccountCreationFailed;
+import com.dang.commonlib.transaction.TransactionSynchronizer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 /**
  * Class responsible for handling messages
@@ -15,21 +20,27 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class AccountHandler {
+    private final TransactionSynchronizer transactionSynchronizer;
+
     @KafkaListener(
             topics = "${com.dang.commonlib.messaging.avrogenerated.accountmanager.AccountCreated.topic}",
             groupId = "pear-store-be-group"
     )
-    public AccountCreated receive(AccountCreated message) {
+    public AccountCreated handleCreated(ConsumerRecord<String, SpecificRecord> message) {
         log.info("Received {}: {}", message.getClass().getCanonicalName(), message);
-        return message;
+        UUID messageId = UUID.fromString(new String(message.headers().toArray()[0].value()));
+        transactionSynchronizer.resume(messageId);
+        return  (AccountCreated) message.value();
     }
 
     @KafkaListener(
             topics = "${com.dang.commonlib.messaging.avrogenerated.accountmanager.AccountCreationFailed.topic}",
             groupId = "pear-store-be-group"
     )
-    public AccountCreationFailed receive(AccountCreationFailed message) {
+    public AccountCreationFailed handleFailed(ConsumerRecord<String, SpecificRecord> message) {
         log.info("Received {}: {}", message.getClass().getCanonicalName(), message);
-        return message;
+        UUID messageId = UUID.fromString(new String(message.headers().toArray()[0].value()));
+        transactionSynchronizer.resume(messageId);
+        return (AccountCreationFailed) message.value();
     }
 }
--- src/test/java/com/dang/pearstorebe/handler/AccountHandlerTest.java
+++ src/test/java/com/dang/pearstorebe/handler/AccountHandlerTest.java
@@ -3,6 +3,9 @@ package com.dang.pearstorebe.handler;
 import avrogenerated.accountmanager.AccountCreated;
 import avrogenerated.accountmanager.AccountCreationFailed;
 import com.dang.pearstorebe.accountService.handler.AccountHandler;
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,13 +22,27 @@ public class AccountHandlerTest {
 
     @Test
     public void receiveAccountCreated_success(){
-        AccountCreated accountCreated = accountHandler.receive(new AccountCreated());
+        ConsumerRecord<String, SpecificRecord> consumerRecord = new ConsumerRecord<>(
+                "",
+                0,
+                0,
+                "AccountCreated",
+                new AccountCreated()
+                );
+        AccountCreated accountCreated = accountHandler.handleCreated(consumerRecord);
         assertThat(accountCreated).isNotNull();
     }
 
     @Test
     public void receiveAccountCreationFailed_success(){
-        AccountCreationFailed accountCreationFailed = accountHandler.receive(new AccountCreationFailed());
+        ConsumerRecord<String, SpecificRecord> consumerRecord = new ConsumerRecord<>(
+                "",
+                0,
+                0,
+                "AccountCreationFailed",
+                new AccountCreationFailed()
+        );
+        AccountCreationFailed accountCreationFailed = accountHandler.handleFailed(consumerRecord);
         assertThat(accountCreationFailed).isNotNull();
     }
 }
